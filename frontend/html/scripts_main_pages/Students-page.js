576e2f274c63f9b33a812753a2fce41e648d6bc2
Plamen Todorov <plamentodorovtodorov07@gmail.com>
2024-07-17 22:53:44 +0300

    overview-page studets assignment list required at student-page.js

--- frontend/html/scripts_main_pages/Students-page.js
+++ frontend/html/scripts_main_pages/Students-page.js
@@ -8,3 +8,49 @@ navItems.forEach((navItem, i) => {
     navItem.className = "nav-item active";
   });
 });
+
+let assignmentsHTML = '';
+//Backend: A list of assingments is required with the following data for the last tree assingments that have been assingned to the student.
+let assignments = [
+  {
+    id: 1,
+    name: "Assignment 1",
+    className: "9a",
+    description: "Something for description",
+    dueDate: "1/1/2001"
+  },{
+    id: 2,
+    name: "Assignment 2",
+    className: "9b",
+    description: "Something for description",
+    dueDate: "2/2/2002"
+  },{
+    id: 3,
+    name: "Assignment 3",
+    className: "9c",
+    description: "Something for description",
+    dueDate: "3/3/2003"
+  }];
+
+  assignments.forEach((assignments)=>{
+    if(assignments.name === ""){
+      return;
+    }
+    else{
+      assignmentsHTML += `
+      <container class="activity-container image-container"> 
+       <div class="overlay">
+        <div class="homework-information">
+         <h3 class="homework-name">Homework: ${assignments.name}</h3>
+         <h3 class="homework-date">Due: ${assignments.dueDate}</h3>
+         <h3 class="class-name">Class: ${assignments.className}</h3>
+         <button class="btn-homework">Open</button>
+         <h3 class="homework-description">Description: ${assignments.description}</h3>
+        </div>
+       </div>
+      </container>
+      `
+      }
+  });
+
+document.querySelector(".js-auto-html-homeworks").innerHTML = assignmentsHTML;
