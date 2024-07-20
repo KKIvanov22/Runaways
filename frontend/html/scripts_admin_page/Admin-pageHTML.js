4480a0671e44400e852c5c86c84f6fdb20802dd0
Plamen Todorov <plamentodorovtodorov07@gmail.com>
2024-07-20 15:28:08 +0300

    Add Admin css and js

--- frontend/html/scripts_admin_page/Admin-pageHTML.js
+++ frontend/html/scripts_admin_page/Admin-pageHTML.js
@@ -8,3 +8,23 @@ navItems.forEach((navItem, i) => {
     navItem.className = "nav-item active";
   });
 });
+
+function showDropdown() {
+  document.getElementById("dropdown-content").style.display = "block";
+};
+
+function hideDropdown() {
+  document.getElementById("dropdown-content").style.display = "none";
+};
+
+function selectOption(value) {
+  document.getElementById("roles").value = value;
+  hideDropdown();
+};
+
+document.addEventListener('click', function(event) {
+  const dropdown = document.getElementById("dropdown-content");
+  if (!dropdown.contains(event.target) && !document.getElementById("roles").contains(event.target)) {
+      hideDropdown();
+  }
+});
